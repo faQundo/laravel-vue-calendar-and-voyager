6bae0b34324349eb01d0aaa2e624d8a4346ebac6
faqundo <facundo.ferro@werden.com.ar>
2021-08-15 14:07:02 -0300

    fix router configuration

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import vuetify from './plugins/vuetify'
+import router from './router/index'
 
 new Vue({
-    /* router,
-    store,
+     router,
+    /*store,
     i18n,
     acl, */
     vuetify,
---
+++ resources/js/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" :src="logo" /> -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "../components/HelloWorld.vue";
+import logo from '../assets/logo.svg';
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    logo: logo,
+  }),
+};
+</script>
